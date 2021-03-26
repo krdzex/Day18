1fc35e974b17fc08d6852c390dc8230f8c3ed9ad
krdzex <58869690+krdzex@users.noreply.github.com>
2021-03-26 08:09:38 +0100

    Small change

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -4,9 +4,9 @@ function* myGenerator() {
     yield* 'Arena';
     yield* arguments;
 }
-var iterator = myGenerator(6, 7, 8);
+let iterator = myGenerator(6, 7, 8);
 let generatorArray = [];
-for (var i = 0; i < 13; i++) {
+for (let i = 0; i < 13; i++) {
     generatorArray.push(iterator.next().value);
 }
 console.log(generatorArray);
